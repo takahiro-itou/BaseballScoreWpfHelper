8811459911fbcc2549b699cdcddc593f0180e767
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-30 12:44:25 +0900

    空のビューモデルクラスを新規作成

---
+++ ViewModels/RankingViewModel.cs
@@ -0,0 +1,23 @@
+﻿//  -*-  coding: utf-8-with-signature-unix     -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Baseball  Score  Project  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+
+namespace  BaseballScoreHelper.ViewModels  {
+
+public  class  RankingViewModel : ViewModelBase
+{
+
+}   //  End class  RankingViewModel
+
+}   //  End of namespace  BaseballScoreHelper.ViewModels
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,71 @@
+﻿//  -*-  coding: utf-8-with-signature-unix     -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Baseball  Score  Project  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace  BaseballScoreHelper.ViewModels  {
+
+public  class  ViewModelBase : INotifyPropertyChanged
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+/**   コンストラクタ。
+**
+**/
+public
+ViewModelBase()
+{
+}
+
+
+//========================================================================
+//
+//    Properties.
+//
+
+//----------------------------------------------------------------
+/**
+**
+**/
+public  event PropertyChangedEventHandler?  PropertyChanged;
+
+
+//========================================================================
+//
+//    Protected Member Functions.
+//
+
+//----------------------------------------------------------------
+/**
+**
+**/
+protected  virtual  void
+raisePropertyChanged(
+        [CallerMemberName]  System.String?  propertyName = null)
+{
+    PropertyChanged?.Invoke(
+            this, new PropertyChangedEventArgs(propertyName));
+}
+
+
+}   //  End class  ViewModelBase
+
+}   //  End of namespace  BaseballScoreHelper.ViewModels
