dd083d2d217842220f907fba2c55371313d85b13
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-31 22:38:23 +0900

    テスト用ダミーデータを用意

--- ViewModels/VictoryLineViewModel.cs
+++ ViewModels/VictoryLineViewModel.cs
@@ -33,6 +33,30 @@ public  class  VictoryLineViewModel : ViewModelBase
 public
 VictoryLineViewModel()
 {
+    //  ダミーデータ。  //
+    System.String   keyHeadCol = "勝数";
+
+    this.m_dtLines  = new DataTable();
+    this.m_dtLines.Columns.Add(keyHeadCol);
+    this.m_dtLines.Columns.Add("Team 1");
+    this.m_dtLines.Columns.Add("Team 2");
+    this.m_dtLines.Columns.Add("Team 3");
+
+    for ( int i = 10; i >= 0; -- i ){
+        var row = this.m_dtLines.NewRow();
+        row[keyHeadCol] = i;
+        if ( i >= 5 ) {
+            row["Team 1"] = "";
+        } else {
+            row["Team 1"] = $"{i}-{5-i}: {(i + 5)/20}"
+        }
+        if ( i >= 6 ) {
+            row["Team 2"] = "";
+        } else {
+            row["Team 2"] = $"{i}-{6-i}: {(i + 4)/20}"
+        }
+        row["Team 3"] = $"{i}-{10-i}: {(i + 1)/20}"
+    }
 }
 
 
