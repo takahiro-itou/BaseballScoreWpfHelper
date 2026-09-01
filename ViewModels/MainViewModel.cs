4c0cbe2fb8d22dde8dac67a5a0ca5e6b47eab6f9
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-01 23:24:05 +0900

    コンパイルエラーを修正

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -15,7 +15,7 @@
 using System.Collections.ObjectModel;
 using System.Windows.Input;
 
-using WpfControl.Common;
+using BaseballScoreHelper.Common;
 using BaseballScoreHelper.Document;
 using BaseballScoreHelper.Services;
 
