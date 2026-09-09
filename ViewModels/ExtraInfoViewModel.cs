2dbd57ed66d6607a5703e72856017be7fc47686e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-09 16:21:03 +0900

    追加情報エリアに表示するデータを切り替えられるように修正

--- ViewModels/ExtraInfoViewModel.cs
+++ ViewModels/ExtraInfoViewModel.cs
@@ -38,7 +38,11 @@ public
 ExtraInfoViewModel()
 {
     //  ダミーデータ。  //
+    this.m_selectIndex  = 1;
     this.m_dtRestGames  = new MatrixInfo();
+    this.m_dtMagicInfo  = new MatrixInfo();
+    this.m_dtWinsTable  = new MatrixInfo();
+    this.m_currentInfo  = this.m_dtRestGames;
 }
 
 
@@ -47,14 +51,83 @@ ExtraInfoViewModel()
 //    Properties.
 //
 
+
+//----------------------------------------------------------------
+/**
+**
+**/
+public  virtual  MatrixInfo
+CurrentInfo  {
+    get { return  this.m_currentInfo; }
+    set {
+        this.m_currentInfo = value;
+        raisePropertyChanged();
+    }
+}
+
+//----------------------------------------------------------------
+/**
+**
+**/
+public  virtual  MatrixInfo
+MagicTable  {
+    get { return  this.m_dtMagicInfo; }
+}
+
+//----------------------------------------------------------------
+/**
+**
+**/
+public  virtual  MatrixInfo
+RestGameTable  {
+    get { return  this.m_dtRestGames; }
+}
+
+//----------------------------------------------------------------
+/**
+**
+**/
+public  virtual  int
+SelectedShowType  {
+    get { return  this.m_selectIndex; }
+    set {
+        if ( this.m_selectIndex != value ) {
+            this.m_selectIndex = value;
+            raisePropertyChanged();
+            updateCurrentInfo();
+        }
+    }
+}
+
+//----------------------------------------------------------------
+/**
+**
+**/
+public  virtual  MatrixInfo
+WinsTable  {
+    get { return  this.m_dtWinsTable; }
+}
+
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
 //----------------------------------------------------------------
 /**
 **
 **/
 
-public  virtual  MatrixInfo
-RestGameTable  {
-    get { return  this.m_dtRestGames; }
+private  void  updateCurrentInfo()
+{
+    this.CurrentInfo = SelectedShowType switch
+    {
+        1 => m_dtRestGames,
+        2 => m_dtMagicInfo,
+        3 => m_dtWinsTable,
+        _ => m_dtRestGames
+    };
 }
 
 
@@ -63,7 +136,15 @@ RestGameTable  {
 //    Member Variables.
 //
 
+private   int           m_selectIndex;
+
+private   MatrixInfo    m_currentInfo;
+
 private   MatrixInfo    m_dtRestGames;
+
+private   MatrixInfo    m_dtMagicInfo;
+
+private   MatrixInfo    m_dtWinsTable;
 
 
 }   //  End class  ExtraInfoViewModel
