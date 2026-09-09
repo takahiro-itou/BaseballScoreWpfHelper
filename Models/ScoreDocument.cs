95142f74d1b6e5f927710b616066c83389fe9305
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-09 17:26:11 +0900

    空のドキュメントクラスを新規作成

---
+++ Models/ScoreDocument.cs
@@ -0,0 +1,65 @@
+﻿//  -*-  coding: utf-8-with-signature-unix     -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  Baseball  Score  Project  ---                  **
+**                                                                      **
+**          Copyright (C), 2017-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+
+namespace  BaseballScoreHelper.Models  {
+
+//========================================================================
+//
+//    ScoreDocument  class
+//
+
+public  class  ScoreDocument
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+/**   コンストラクタ。
+**
+**/
+public  ScoreDocument()
+{
+}
+
+
+//========================================================================
+//
+//    Public Member Functions.
+//
+
+
+//========================================================================
+//
+//    Properties.
+//
+
+
+//========================================================================
+//
+//    For Internal Use Only.
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+
+}   //  End of class  ScoreDocument
+
+}   //  End of namespace  BaseballScoreHelper.Models
