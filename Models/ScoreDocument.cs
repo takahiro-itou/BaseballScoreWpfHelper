d062715c37782004d47401cc577240d62d21d21d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-13 11:49:04 +0900

    モデルクラスに SelectedDate プロパティを実装

--- Models/ScoreDocument.cs
+++ Models/ScoreDocument.cs
@@ -48,6 +48,10 @@ public  ScoreDocument()
     this.m_docScore     = new WrapDocument.ScoreDocument();
     this.m_leagueInfos  = new ObservableCollection<LeagueInfo>();
     this.m_scoreInfos   = new DocumentSummary[1, NUM_MAGIC_MODES];
+
+    this.m_selectedLeague = 0;
+    this.m_magicMode    = 0;
+    this.m_selectedDate = DateTime.Today;
 }
 
 
@@ -90,34 +94,54 @@ summarizeDocument(
 //    Properties.
 //
 
+//----------------------------------------------------------------
+/**   プロパティ  LastActiveDate
+**
+**/
 public  virtual  System.DateTime
 LastActiveDate  {
     get { return  this.m_docScore.LastActiveDate; }
 }
 
+//----------------------------------------------------------------
+/**   プロパティ  LastRecordDate
+**
+**/
 public  virtual  System.DateTime
 LastRecordDate  {
     get { return  this.m_docScore.LastRecordDate; }
 }
 
-
+//----------------------------------------------------------------
+/**   プロパティ  Leagues
+**
+**/
 public  virtual  ObservableCollection<LeagueInfo>
 Leagues  {
     get { return  this.m_leagueInfos; }
 }
 
-
-public  virtual  int
-SelectedMagicMode  {
-    get { return  this.m_magicMode; }
+//----------------------------------------------------------------
+/**   プロパティ  SelectedDate
+**
+**/
+public  virtual  System.DateTime?
+SelectedDate  {
+    get { return  this.m_selectedDate; }
     set {
-        if ( this.m_magicMode != value ) {
-            this.m_magicMode = value;
+        if ( this.m_selectedDate != value ) {
+            this.m_selectedDate = value;
+            summarizeDocument(value);
             notifyLeagueSummaryChanged();
         }
-    }
+   }
 }
 
+
+//----------------------------------------------------------------
+/**   プロパティ  SelectedLeagueIndex
+**
+**/
 public  virtual  int
 SelectedLeagueIndex  {
     get { return  this.m_selectedLeague; }
@@ -129,10 +153,29 @@ SelectedLeagueIndex  {
     }
 }
 
+//----------------------------------------------------------------
+/**   プロパティ  SelectedLeagueSummary
+**
+**/
 public  virtual  DocumentSummary
 SelectedLeagueSummary  {
     get {
         return  this.m_scoreInfos[this.m_selectedLeague, this.m_magicMode];
+    }
+}
+
+//----------------------------------------------------------------
+/**   プロパティ  SelectedMagicMode
+**
+**/
+public  virtual  int
+SelectedMagicMode  {
+    get { return  this.m_magicMode; }
+    set {
+        if ( this.m_magicMode != value ) {
+            this.m_magicMode = value;
+            notifyLeagueSummaryChanged();
+        }
     }
 }
 
@@ -206,7 +249,7 @@ updateInfos()
         this.m_leagueInfos.Add(this.m_docScore.getLeagueInfo(i));
     }
 
-    summarizeDocument(System.DateTime.Now);
+    summarizeDocument(this.m_selectedDate);
     notifyLeagueListChanged();
     notifySelectedLeagueChanged();
     notifyLeagueSummaryChanged();
@@ -232,6 +275,8 @@ private   ObservableCollection<LeagueInfo>      m_leagueInfos;
 private   int                                   m_selectedLeague;
 
 private   int                                   m_magicMode;
+
+private   System.DateTime?                      m_selectedDate;
 
 private   DocumentSummary[,]                    m_scoreInfos;
 
